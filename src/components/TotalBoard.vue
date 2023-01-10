4ef9a1f1faec427e9b5f085b4ba0fe81abce7867
Boldinov90 <71153593+Boldinov90@users.noreply.github.com>
2023-01-10 14:21:49 +0300

    s90

--- src/components/TotalBoard.vue
+++ src/components/TotalBoard.vue
@@ -1,5 +1,21 @@
 <template>
-   <div class="total-board">khkjhkjhkjh</div>
+   <div class="total-board">
+      <div class="total-board__title">Итого</div>
+      <div class="total-board__order-information">
+         <div class="order-information__order-amount">
+            Сумма заказа
+            <span>50 576 ₽</span>
+         </div>
+         <div class="order-information__order-count">
+            Количество
+            <span>4 шт</span>
+         </div>
+         <div class="order-information__order-installation">
+            Установка
+            <span>Нет</span>
+         </div>
+      </div>
+   </div>
 </template>
 
 <script>
